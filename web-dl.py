f8fcecfb36e65b3fcc5637ad08073e83d7b82979
Rafael Herrero <rafahsolis@gmail.com>
2015-02-09 13:09:26 -0800

    random download added for classification.json

--- web-dl.py
+++ web-dl.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 import sys
+import random
+import json
+import requests
 
 def multipleUrldl(filename):
     '''
@@ -13,11 +16,50 @@ def multipleUrldl(filename):
         os.system("wget " + url)
 
 
+def rnd1000(filename):
+    newfilenamestd = "web-dl"
+    exists = os.path.isfile("web-dl.json")
+    counter = 0
+    while exists:
+        newfilename = newfilenamestd + str(counter)
+        exists = os.path.isfile(newfilename + ".json")
+        counter += 1
+    if counter < 1:
+        newfilename = newfilenamestd + ".json"
+    else:
+        newfilename = newfilename + ".json"
+
+    sample = random.sample(range(0, 26908), 1000)
+    counter = 0
+    urlsfile = open(filename, 'r')
+
+    with open(newfilename, 'w') as resultfile:
+        data = json.load(urlsfile)
+        for i in data:
+            if counter in sample:
+                try:
+                    web = requests.get(i['URL'])
+                    i['html'] = web.text
+                except Exception as e:
+                    print e
+                    continue
+
+                finally:
+                    json.dump(i, resultfile)
+                    resultfile.write('\n')
+
+            counter += 1
+        urlsfile.close()
+
+
 def main():
     if len(sys.argv) == 2:
         multipleUrldl(sys.argv[1])
+    elif sys.argv[2] == "--rnd1000":
+        rnd1000(sys.argv[1])
     else:
-        print "ussage: python web-dl filename"
+        print "ussage: python web-dl filename {--rnd1000; this parameter will download a random 1000 pages from the " \
+              "26900 contained in classification.json adding a json field html:value} "
 
 if __name__ == '__main__':
     main()
